41f497b92b3fcb2da6a22ba6080398432e36c5fa
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2019-07-05 09:59:41 -0400

    Made the button store variables in javascript

--- designs.js
+++ designs.js
@@ -4,7 +4,18 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    const width = document.getElementById('inputHeight').value;
+    const height = document.getElementById('inputWidth').value;
 
-// Your code goes here!
+    for (let rows = 0; rows < width; rows++) {
+        for (let columns = 0; columns < height; columns++) {
+            var node = document.createElement("div");
+            node.setAttribute('class', 'grid')
+            document.getElementById('container').appendChild(node);
+        };
+    };
+    $(".grid").width(480/width);
+    $(".grid").height(480/height);
+};
 
-}
+
