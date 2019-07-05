3c2c8105933a6be96581596676dcb87bfe07d347
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2019-07-05 11:37:33 -0400

    Added a table when the submit button is clicked

--- designs.js
+++ designs.js
@@ -7,15 +7,10 @@ function makeGrid() {
     const width = document.getElementById('inputHeight').value;
     const height = document.getElementById('inputWidth').value;
 
-    for (let rows = 0; rows < width; rows++) {
-        for (let columns = 0; columns < height; columns++) {
-            var node = document.createElement("div");
-            node.setAttribute('class', 'grid')
-            document.getElementById('container').appendChild(node);
+    for (let row = 0; row < width; row++) {
+        var x = document.getElementById('pixelCanvas').insertRow(row);
+        for (let column = 0; column < height; column++) {
+            x.insertCell(column);
         };
     };
-    $(".grid").width(480/width);
-    $(".grid").height(480/height);
 };
-
-
