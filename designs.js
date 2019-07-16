93f86d61de78f1b0e0d26c09574a507b89e1d9b0
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2019-07-16 18:21:18 -0400

    Fixed board creation

--- designs.js
+++ designs.js
@@ -1,16 +1,34 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-    const width = document.getElementById('inputHeight').value;
-    const height = document.getElementById('inputWidth').value;
-
-    for (let row = 0; row < width; row++) {
-        var x = document.getElementById('pixelCanvas').insertRow(row);
-        for (let column = 0; column < height; column++) {
-            x.insertCell(column);
+class PixelArt {
+    constructor() {
+        this.color = document.getElementById('colorPicker');
+    }
+    makeGrid() {
+        this.deleteGrid();
+        let width = document.getElementById('inputHeight').value;
+        let height = document.getElementById('inputWidth').value;
+    
+        for (let row = 0; row < width; row++) {
+            var x = document.getElementById('pixelCanvas').insertRow(row);
+            for (let column = 0; column < height; column++) {
+                var z = x.insertCell(column);
+                z.addEventListener("click", function () {
+                    this.setAttribute("style", `background-color: ${color.value}`)
+                })
+            };
         };
+        var table = document.getElementById("pixelCanvas");
+        table.deleteRow(-1)
     };
-};
+
+    deleteGrid() {
+        var table = document.getElementById("pixelCanvas");
+        for(var i = table.rows.length - 1; i > 0; i--)
+        {
+            table.deleteRow(i);
+        }
+    }
+    
+}
+
+let pixels = new PixelArt();
+
